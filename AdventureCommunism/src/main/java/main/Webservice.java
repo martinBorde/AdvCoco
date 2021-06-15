666304353f92b6656e23b2f2f06f31b25a7d89e5
ClJankowiak <65659527+ClJankowiak@users.noreply.github.com>
2021-06-15 15:16:13 +0200

    Update Webservice.java

--- AdventureCommunism/src/main/java/main/Webservice.java
+++ AdventureCommunism/src/main/java/main/Webservice.java
@@ -1,9 +1,17 @@
 package main;
+import javax.print.attribute.standard.Media;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import generated.PallierType;
+import generated.ProductType;
 
 @Path("generic")
 public class Webservice {
@@ -15,8 +23,25 @@ public class Webservice {
 
     @GET
     @Path("world")
-    @Produces(MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON)
-    public Response getWorld() {
-        return Response.ok(services.getWorld()).build();
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getWorld(@Context HttpServletRequest request) {
+        String username = request.getHeader("X-user");
+        return Response.ok(services.getWorld(username)).build();
+    }
+
+    @PUT
+    @Path("product")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public boolean putProduct(@Context HttpServletRequest request, ProductType product) {
+        String username = request.getHeader("X-user");
+        return services.updateProduct(username, product);
+    }
+
+    @PUT
+    @Path("manager")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public boolean putManager(@Context HttpServletRequest request, PallierType manager) {
+        String username = request.getHeader("X-user");
+        return services.updateManager(username, manager);
     }
 }
